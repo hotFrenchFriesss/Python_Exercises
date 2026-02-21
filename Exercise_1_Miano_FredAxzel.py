531bcff260cbab921cc100875c0c0a9bbe805e47
Fred Axzel Miano <genjen580@gmail.com>
2026-02-21 16:44:20 +0800

    First Python exercises created

---
+++ Exercise_1_Miano_FredAxzel.py
@@ -0,0 +1,7 @@
+
+#Name, Section, Course, Subject
+
+print("Name: Miano, Fred Axzel")
+print("Section: TX22")
+print("Course: BSITAGD")
+print("Subject: IT0011 - Integrative Programming and Technologies")
